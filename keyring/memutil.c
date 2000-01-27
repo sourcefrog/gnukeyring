1d7e1e014787dfde965e34e4713973261f6e09db
Martin Pool <mbp@sourcefrog.net>
2000-01-27 07:31:47 +0000

    Initial revision

---
+++ keyring/memutil.c
@@ -0,0 +1,142 @@
+/* -*- mode: c; c-indentation-style: "k&r"; c-basic-offset: 4 -*-
+ * $Id$
+ * 
+ * GNU Tiny Keyring for PalmOS -- store passwords securely on a handheld
+ * Copyright (C) 1999 Martin Pool
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <Pilot.h>
+#include <Password.h>
+#include <Encrypt.h>
+
+#include "keyring.h"
+#include "memutil.h"
+#include "callback.h"
+
+// ======================================================================
+// Memory management routines
+
+/* Scribble over all memory allocated to a handle.  It's OK to pass a null
+ * handle */
+void Mem_ObliterateHandle(VoidHand h) {
+    CharPtr ptr;
+
+    if (!h)
+	return;
+
+    ptr = MemHandleLock(h);
+    Mem_ObliteratePtr(ptr);
+   
+    MemHandleUnlock(h);
+}
+
+
+void Mem_ObliteratePtr(VoidPtr ptr) {
+    ULong size = MemPtrSize(ptr);
+    if (!size)
+	return;
+    MemSet(ptr, size-1, 'X');
+    ((CharPtr) ptr)[size-1] = '\0';
+}
+
+
+/* Return handle of a new chunk containing a copy of the string at
+ * SRCPTR, and also put the length of that string in *LEN.  If the string
+ * is "", then return null.  The returned length does not include the
+ * terminal NUL, but the allocated chunk does have space for it. */
+VoidHand Mem_StrToHandle(CharPtr srcPtr, ULong *len) {
+    VoidHand h;
+    VoidPtr destPtr;
+    
+    if ((*len = StrLen(srcPtr)) > 0) {    
+	h = MemHandleNew(*len + 1);
+	ErrFatalDisplayIf(!h, __FUNCTION__ ": out of memory");
+	destPtr = MemHandleLock(h);
+	StrCopy(destPtr, srcPtr);
+	MemHandleUnlock(h);
+	return h;
+    } else {
+	return 0;
+    }
+}
+
+
+VoidHand Mem_ReadString(CharPtr *ptr, ULongPtr len) {
+    VoidHand h;
+    
+    h = Mem_StrToHandle(*ptr, len);
+    *ptr += *len + 1;
+    return h;
+}
+
+
+void Mem_ReadChunk(CharPtr *ptr, ULong len, VoidPtr dest) {
+    MemMove(dest, *ptr, len);
+    *ptr += len;
+}
+
+
+void DB_WriteStringFromHandle(CharPtr dest, ULong *off, VoidHand h, ULong len) {
+    if (h) {
+	CharPtr p = MemHandleLock(h);
+	DmWrite(dest, *off, p, len);
+	*off += len;
+	MemHandleUnlock(h);
+    } else {
+	DmWrite(dest, *off, "", 1);
+	(*off)++;
+    }
+}
+
+
+void Mem_CopyFromHandle(CharPtr *dest, VoidHand h, ULong len)
+{
+    if (h) {
+	CharPtr p = MemHandleLock(h);
+	MemMove(*dest, p, len);
+	*dest += len;
+	MemHandleUnlock(h);
+    } else {
+	**dest = 0;
+	*dest += 1;
+    }
+}
+
+
+Word DB_ReadWord(CharPtr *ptr) {
+    return *((Word *) ptr)++;
+}
+
+
+ULong DB_ReadULong(CharPtr *ptr) {
+    return *((ULong *) ptr)++;
+}
+
+
+/* Read a field value from *PTR, which is a pointer into a read-only
+ * database record.  PTR advances over the read value.  If the string
+ * is empty, the field keeps using it default buffer. */
+void Mem_ReadStringIntoField(CharPtr *recPtr, FormPtr frm, Word id) {
+    VoidHand tmpHandle;
+    ULong recLen;
+    FieldPtr fld;
+    
+    tmpHandle = Mem_StrToHandle(*recPtr, &recLen);
+    *recPtr += recLen + 1;
+    fld = FrmGetObjectPtr(frm, FrmGetObjectIndex(frm, id));
+    FldSetTextHandle(fld, (Handle) tmpHandle);
+}
---
+++ keyring/resource.h
@@ -0,0 +1,93 @@
+/* -*- mode: c; c-indentation-style: "k&r"; c-basic-offset: 4 -*-
+ * Copyright 1999 by Martin Pool
+ * $Id$
+ *
+ * Keyring for PalmOS
+ */
+
+#define ListForm			1000
+#define KeyEditForm			1001
+#define AboutForm			1002
+#define UnlockForm			1003
+#define WrongKeyAlert			1004
+#define SysErrAlert			1005
+#define ChecksumForm			1006
+#define SecurityForm			1007
+#define NotImplementedAlert		1008
+#define ConfirmDeleteForm		1009
+#define SetPasswdForm			1010
+#define ConfirmPasswdForm		1011
+#define PasswdMismatchAlert		1012
+#define PrefsForm			1013
+#define InitForm			1014
+#define BusyEncryptForm			1015
+#define BusyDecryptForm			1016
+#define GenerateForm			1017
+
+#define ListMenuBar			1100
+#define KeyEditMenuBar			1101
+#define GnuBitmap			1102
+#define KeyringBitmap			1103
+
+#define KeysList			1200
+#define KeyNameField			1201
+#define DoneBtn				1202
+#define KeysListScrollBar		1203
+#define CountDownTrigger		1204
+#define LockBtn				1205
+#define UnlockBtn			1206
+#define CancelBtn			1207
+#define SecurityBtn			1208
+#define MasterKeyFld			1209
+#define DateTrigger			1210
+#define NewKeyBtn			1211
+#define AccountField			1212
+#define PasswordField			1213
+#define MessageField			1214
+#define MD5Push				1215
+#define MD4Push				1216
+#define SumField			1217
+#define SumBtn				1218
+#define OkBtn				1219
+#define SaveArchiveCheck		1220
+#define NotesField			1221
+#define NotesScrollbar			1222
+#define TitleTemplateStr		1223
+#define EmptyTitleStr			1224
+#define Expiry0Push			1225
+#define Expiry15Push			1226
+#define Expiry60Push			1227
+#define Expiry300Push			1228
+#define ConfirmFld			1229
+#define LengthFld			1230
+#define Length4Push			1231
+#define Length6Push			1232
+#define Length8Push			1233
+#define Length10Push			1234
+#define Length16Push			1235
+#define Length20Push			1236
+#define IncludeLower			1237
+#define IncludeUpper			1238
+#define IncludeDigits			1239
+#define IncludePunct			1240
+#define GenerateBtn			1241
+
+#define AboutCmd			1300
+#define KeyboardCmd			1301
+#define GraffitiReferenceCmd		1302
+#define ChecksumCmd			1303
+#define SecurityCmd			1304
+#define DeleteKeyCmd			1305
+#define SetPasswdCmd			1306
+#define PrefsCmd			1307
+#define GenerateCmd			1308
+
+#define EditCopy			1400
+#define EditCut				1401
+#define EditPaste			1402
+#define EditUndo			1403
+#define EditSelectAll			1404
+
+#define ConfirmDeleteStr		1501
+#define GenerateHelp			1502
+#define PreferencesHelp			1503
