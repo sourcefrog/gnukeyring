7a2530061818751820d9caa2cda85036df7e90aa
mbp <mbp@80d6c7a2-0132-0410-bcb4-b0b62b5cc441>
2000-08-31 11:15:37 +0000

    Move towards storing the session key and master password hash in hidden records rather than AppInfo or SortInfo.  Not complete yet, but checks the password hash.

--- trunk/keyring/crypto.c
+++ trunk/keyring/crypto.c
@@ -1,8 +1,8 @@
 /* -*- mode: c; c-indentation-style: "k&r"; c-basic-offset: 4 -*-
  * $Id$
  * 
- * GNU Tiny Keyring for PalmOS -- store passwords securely on a handheld
- * Copyright (C) 1999, 2000 Martin Pool
+ * GNU Keyring for PalmOS -- store passwords securely on a handheld
+ * Copyright (C) 1999, 2000 Martin Pool <mbp@humbug.org.au>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,40 +30,47 @@
 // ======================================================================
 // DES3 functions
 
-#undef DISABLE_DES
+#define DISABLE_DES
 
+#ifndef DISABLE_DES
 Err DES3_Buf(void * from, void * to, UInt32 len, Boolean encrypt,
 	     UInt8 const *key)
 {
-    UInt8	other[kBlockSize];
+    UInt8	other[kDESBlockSize];
     Err		err = 0;
     
-    ErrNonFatalDisplayIf(len & (kBlockSize-1),
+    ErrNonFatalDisplayIf(len & (kDESBlockSize-1),
 			 __FUNCTION__ ": not block padded");
     ErrFatalDisplayIf(!to || !from, __FUNCTION__ ":null");
 
     do {
-#ifndef DISABLE_DES	
 	err = EncDES(from, (UInt8 *) key, to, encrypt);
 	if (err)
 	    return err;
 
-	err = EncDES(to, (UInt8 *) key+kBlockSize, other, !encrypt);
+	err = EncDES(to, (UInt8 *) key+kDESBlockSize, other, !encrypt);
 	if (err)
 	    return err;
 
 	err = EncDES(other, (UInt8 *) key, to, encrypt);
 	if (err)
 	    return err;
-#else /* DISABLE_DES */
-	MemMove(to, from, kBlockSize);
-#endif /* DISABLE_DES */
-	to += kBlockSize;
-	from += kBlockSize;
-	len -= kBlockSize;
+
+	to += kDESBlockSize;
+	from += kDESBlockSize;
+	len -= kDESBlockSize;
     } while (len > 0);
 
     return 0;
 }
 
+#else /* DISABLE_DES */
 
+Err DES3_Buf(void * from, void * to, UInt32 len, Boolean UNUSED(encrypt),
+	     UInt8 const UNUSED(*key))
+{
+    MemMove(to, from, len);
+
+    return 0;
+}
+#endif /* DISABLE_DES */
---
+++ trunk/keyring/pwhash.c
@@ -0,0 +1,97 @@
+/* -*- c-indentation-style: "k&r"; c-basic-offset: 4; indent-tabs-mode: t; -*-
+ *
+ * $Id$
+ * 
+ * GNU Keyring for PalmOS -- store passwords securely on a handheld
+ * Copyright (C) 1999, 2000 Martin Pool <mbp@humbug.org.au>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <PalmOS.h>
+
+#include "keyring.h"
+#include "keydb.h"
+#include "crypto.h"
+#include "pwhash.h"
+#include "resource.h"
+
+/*
+ * Store the checking-hash of a password into record[kMasterHashRec].
+ */
+Err PwHash_Store(Char *newPasswd)
+{
+    Err			err;
+    Char		digest[kMD5HashSize];
+    MemHandle		recHandle;
+    void		*recPtr;
+    Int16		idx;
+
+    err = EncDigestMD5(newPasswd, StrLen(newPasswd), digest);
+    if (err) {
+	App_ReportSysError(CryptoErrorAlert, err);
+	return err;
+    }
+
+    idx = kMasterHashRec;
+    recHandle = DmNewRecord(gKeyDB, &idx, kMD5HashSize);
+    if (!recHandle) {
+	App_ReportSysError(KeyDatabaseAlert, DmGetLastErr());
+	return err;
+    }
+
+    recPtr = MemHandleLock(recHandle);
+
+    DmWrite(recPtr, 0, digest, kMD5HashSize);
+
+    MemHandleUnlock(recHandle);
+
+    return 0;
+}
+
+
+/*
+ * Check whether GUESS is the correct password for the database.
+ */
+Boolean PwHash_Check(Char *guess)
+{
+    Char		digest[kMD5HashSize];
+    MemHandle		recHandle;
+    Char		*recPtr;
+    Boolean		result;
+    Err			err;
+
+    /* Compute the hash of the entered password. */
+    err = EncDigestMD5(guess, StrLen(guess), digest);
+    if (err) {
+	App_ReportSysError(CryptoErrorAlert, err);
+	return false;
+    }
+
+    /* Retrieve the hash record. */
+    recHandle = DmQueryRecord(gKeyDB, kMasterHashRec);
+    if (!recHandle) {
+	App_ReportSysError(KeyDatabaseAlert, err);
+	return false;
+    }
+    recPtr = MemHandleLock(recHandle);
+
+    result = !MemCmp(digest, recPtr, kMD5HashSize);
+    MemHandleUnlock(recHandle);
+
+    return result;
+}
+
+
