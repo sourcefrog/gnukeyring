cf3b451d13cf449ca25b1468b4673e25579ec384
Martin Pool <mbp@sourcefrog.net>
2000-09-28 01:28:43 +0000

    More notes on password generation

--- keyring/speakable.py
+++ keyring/speakable.py
@@ -1,14 +1,38 @@
 #! /usr/bin/python
 
+# $Id$
+
+# Prototype of generating pronounceable passwords.  This one works OK
+# - the passwords produced are pronounceable, but they do not look
+# much like English.
+
+# Maybe a Markov model constructed from actual
+# text would be better?  We can't afford too large a table, though: if
+# we use just 26 letters plus space, and map from the previous two
+# characters to probabilities of the next, then we'd use 27**3 bytes =
+# 20k, which is far too much for PalmOS.  There are 27**2 == 729
+# precursor states, but many of them will be empty.
+
+# Perhaps we should explicitly encode the states we've seen, and make
+# sure they're fully connected?  Just store for each state a list of
+# possible transitions out, and for each the character to emit, the
+# probability, and the next state number.  I think then we could make
+# the table any arbitrary length.
+
 import random
 
-consonants = ['qu', 'w', 'r',
-              't', 'th', 'tr', 
-              'y',
+consonants = [
+              'b', 'bl',
+              'c', 'ch', 'cl', 'cr',
+              'd', 'dr',
+              'f',
+              'g', 'gr', 
+              'h', 'j', 'k', 'l', 'm', 'n',
               'p', 'pr', 'pl',
-              's', 'sh', 'st', 'd', 'f', 'g',
-              'h', 'j', 'k', 'l', 'z', 'x', 'c',
-              'ch', 'cl', 'cr', 'v', 'b', 'n', 'm']
+              'qu', 'r',
+              's', 'sh', 'sl', 'sp', 'spr', 'st', 'str', 'sm',
+              't', 'th', 'tr', 'tw',
+              'v', 'w', 'y', 'z' ]
 vowels = ['a', 'e', 'i', 'o', 'u']
 
 for i in range(100):
