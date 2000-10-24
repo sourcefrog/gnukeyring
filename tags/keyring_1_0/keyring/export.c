d5ac7fcaeccbf8e8340bfa5241b4cc25bb3924a8
nobody <nobody@80d6c7a2-0132-0410-bcb4-b0b62b5cc441>
2000-10-24 11:13:50 +0000

    This commit was manufactured by cvs2svn to create tag 'keyring_1_0'.

---
+++ tags/keyring_1_0/keyring/export.c
@@ -0,0 +1,241 @@
+/* -*- c-file-style: "k&r"; -*-
+ *
+ * $Id$
+ * 
+ * GNU Keyring -- store passwords securely on a handheld
+ * Copyright (C) 1999, 2000 Martin Pool <mbp@humbug.org.au>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+ * Export to Memo Pad function.
+ *
+ * Memo Pad records are limited to 4kB.  Since all our fields have
+ * limits than sum to less than that we should always be OK.
+ */
+
+/*
+ * FIXME: - Selecting a record to export to memo results in
+ * "...possible memory leak...". The only time in my limited Palm
+ * programming I ran into this was because all forms weren't closed
+ * before exiting my app.  I'm not sure this is really a problem, as
+ * memory should be given back by the fact Keyring has exited.  -- Dell
+ *
+ * TODO: Export using localized field headings, or perhaps don't
+ * export the headings at all.
+ */
+
+#include <PalmOS.h>
+#include <Password.h>
+#include <Encrypt.h>
+
+#include "resource.h"
+#include "keyring.h"
+#include "keydb.h"
+#include "record.h"
+#include "util.h"
+#include "memutil.h"
+#include "dbutil.h"
+#include "export.h"
+
+/*
+ * Maximum length of record supported by the Memo Pad application
+ */
+#define kMaxExport (4<<10)
+
+/*
+ * Database constants to access the built-in Memo Pad.
+ */
+#define kMemoType 'DATA'
+#define kApplType 'appl'
+#define kMemoCreator 'memo'
+
+
+static void Export_Failure(void)
+{
+    FrmAlert(MemoDatabaseErrorAlert);
+}
+
+
+/*
+ * Write the exported form of an unpacked record into a pre-allocated
+ * space in a memo record.  Returns the number of bytes written, which
+ * will be the new length of the memo record.
+ */
+static UInt16 Export_BuildText(UnpackedKeyType *keyRecord,
+			       void *memoRecord)
+{
+    UInt32	off;
+
+    /* XXX: Ugh, ugly.  Try rewriting this more concisely. */
+
+    off = 0;
+
+    /* NB it's OK to call this with a null handle. */
+    DB_WriteStringFromHandle(memoRecord, &off, keyRecord->nameHandle,
+                             keyRecord->nameLen);
+    if (keyRecord->acctHandle) {
+	DB_WriteString(memoRecord, &off, "\nAccount: ");
+	DB_WriteStringFromHandle(memoRecord, &off, keyRecord->acctHandle,
+				 keyRecord->acctLen);
+    }
+    if (keyRecord->passwdHandle) {
+	DB_WriteString(memoRecord, &off, "\nPassword: ");
+	DB_WriteStringFromHandle(memoRecord, &off, keyRecord->passwdHandle,
+				 keyRecord->passwdLen);
+    }
+    if (keyRecord->notesHandle) {
+	DB_WriteString(memoRecord, &off, "\n\n");
+	DB_WriteStringFromHandle(memoRecord, &off, keyRecord->notesHandle,
+				 keyRecord->notesLen);
+    }
+
+    DmWrite(memoRecord, off, "", 1); /* write nul */
+    off++;
+    return (UInt16) off;
+}
+
+
+/*
+ * Create a new memo to hold an exported record, and return a locked pointer
+ * thereto.
+ */
+static void* Export_CreateMemo(DmOpenRef *dbp, Int16 *pidx, LocalID *memoDbID,
+			       UInt16 *memoDbCard)
+{    
+    UInt32	recLen;
+    Err		err;
+    MemHandle	newHandle;
+    void *     recPtr;
+
+    if (!(*dbp = DmOpenDatabaseByTypeCreator(kMemoType, kMemoCreator, dmModeReadWrite)))
+	goto failOut;
+
+    err = DmOpenDatabaseInfo(*dbp, memoDbID, NULL, NULL, memoDbCard, NULL);
+    if (err)
+	goto failClose;
+
+    recLen = kMaxExport;
+    *pidx = dmMaxRecordIndex;
+    newHandle = DmNewRecord(*dbp, pidx, recLen);
+    if (!newHandle) 
+	goto failClose;
+
+    recPtr = MemHandleLock(newHandle);
+    if (!recPtr) 
+	goto failDelete;
+	
+    return recPtr;
+    
+
+ failDelete:
+    DmDeleteRecord(*dbp, *pidx);    
+    
+ failClose:
+    DmCloseDatabase(*dbp);
+
+ failOut:
+    return NULL;
+}
+
+
+
+/*
+ * Finish off the memo record: resize, unlock, and commit.
+ */
+static Int16 Export_Finish(DmOpenRef dbp, Int16 idx, Int16 size, void *recPtr)
+{
+    Boolean	dirty = true;
+    
+    MemPtrUnlock(recPtr);
+    DmReleaseRecord(dbp, idx, dirty);
+
+    if (!(DmResizeRecord(dbp, idx, size))) {
+         /* XXX: Perhaps we should do this before releasing the
+          * record?  I don't think it matters. */
+	return 0;
+    }
+    
+    DmCloseDatabase(dbp);
+    return 1;
+}
+
+
+/* Jump to the just-created memo. */
+static int Export_JumpToMemo(LocalID memoDbID, UInt16 memoDbCard, Int16 idx) {
+    UInt16		appCard = 0;
+    Err			err;
+    LocalID		appID;
+    GoToParamsPtr	params;
+    UInt16		cmd = 0;
+    DmSearchStateType	state;
+
+    /* Work out the database ID for the MemoPad application and data. */
+    err = DmGetNextDatabaseByTypeCreator(true, &state, kApplType,
+					 kMemoCreator, true, &appCard,
+					 &appID);
+    if (err) {
+	FrmAlert(CouldntLaunchMemoAlert);
+	return 0;
+    }
+    
+    /* Construct a GoToParameters pointing to this record. */
+    if (!(params = MemPtrNew(sizeof *params))) {
+	FrmAlert(OutOfMemoryAlert);
+	return 0;
+    }
+    MemSet(params, sizeof *params, 0);
+    params->dbCardNo = memoDbCard;
+    params->dbID = memoDbID;
+    params->recordNum = idx;
+        
+    /* Give ownership of the PBP to the operating system, so that it
+     * is not freed when we exit.
+     *
+     * Who does free it in this case?  Are we sure it won't leak? */
+    err = MemPtrSetOwner(params, 0);
+    if (err) {
+	FrmAlert(CouldntLaunchMemoAlert);
+	return 0;
+    }
+
+    cmd = sysAppLaunchCmdGoTo;
+    err = SysUIAppSwitch(appCard, appID, cmd, params);
+
+    return 1;
+}
+
+
+/* Export the current key to a MemoPad record. */
+void ExportKey(UnpackedKeyType *keyRecord)
+{
+    void	*memoRecord;
+    Int16	idx;
+    Int16	size;
+    DmOpenRef	dbp;
+    LocalID	memoDbID;
+    UInt16	memoDbCard;
+
+    if (!(memoRecord = Export_CreateMemo(&dbp, &idx, &memoDbID, &memoDbCard))) {
+	Export_Failure();
+    } else if (!(size = Export_BuildText(keyRecord, memoRecord))) {
+	Export_Failure();
+    } else if (!Export_Finish(dbp, idx, size, memoRecord)) {
+	Export_Failure();
+    } else {
+	Export_JumpToMemo(memoDbID, memoDbCard, idx);
+    }
+}
---
+++ tags/keyring_1_0/keyring/pack.c
@@ -0,0 +1,233 @@
+/* -*- c-file-style: "k&r"; -*-
+ *
+ * $Id$
+ * 
+ * GNU Keyring -- store passwords securely on a handheld
+ * Copyright (C) 1999, 2000 Martin Pool <mbp@humbug.org.au>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+    
+/* XXX: It seems there is some kind of bug here in resizing
+ * the record, but I don't know what it is yet.
+ *
+ * From the Programmer's Companion: ``To resize a record to
+ * grow or shrink its contents, call DmResizeRecord. This
+ * routine automatically reallocates the record in another
+ * heap of the same card if the current heap does not have
+ * enough space for it. Note that if the data manager needs to
+ * move the record into another heap to resize it, the handle
+ * to the record changes. DmResizeRecord returns the new
+ * handle to the record.''
+ *
+ * ``May display a fatal error message if any of the following
+ * occur: [] You don t have write access to the database. []
+ * The index parameter is out of range. [] The record chunk is
+ * locked.''
+ *
+ * Alternatively I wonder if something really strange is happening
+ * here, like perhaps we're running out of heap or dynamic memory.
+ * Certainly the encryption stuff might be a bit
+ * memory-intensive.
+ *
+ * TODO: Check record is released before resizing.
+ *
+ * TODO: Better error checking that e.g. we're not running past the
+ * end of the record.  Display an error dialog rather than crashing.
+ */
+
+#include <PalmOS.h>
+
+#include "keyring.h"
+#include "record.h"
+#include "pack.h"
+#include "crypto.h"
+#include "auto.h"
+#include "resource.h"
+#include "memutil.h"
+#include "dbutil.h"
+#include "keydb.h"
+#include "uiutil.h"
+#include "snib.h"
+
+
+static UInt32 packBodyLen, packRecLen;
+
+
+/*
+ * Calculate and store into packRecLen and packBodyLen the amount of
+ * database space this key will use.
+ */
+void Keys_CalcPackedSize(UnpackedKeyType const *unpacked)
+{
+    packBodyLen = unpacked->acctLen + 1
+         + unpacked->passwdLen + 1
+         + unpacked->notesLen + 1;
+
+    if (packBodyLen & (kDESBlockSize-1))
+	packBodyLen = (packBodyLen & ~(kDESBlockSize-1)) + kDESBlockSize;
+
+    packRecLen = unpacked->nameLen + 1 + packBodyLen;
+}
+
+
+/*
+ * Return a newly-allocated buffer containing a packed form of the
+ * body of this key.  The caller should encrypt and store it, then
+ * free the buffer.
+ */
+static char *Keys_PackBody(UnpackedKeyType const *u)
+{
+    char      *buf, *ptr;
+
+    ptr = buf = MemPtrNew(packBodyLen);
+    ErrFatalDisplayIf(!buf, "Not enough dynamic memory to encode record");
+
+    Mem_CopyFromHandle(&ptr, u->acctHandle, u->acctLen+1);
+    Mem_CopyFromHandle(&ptr, u->passwdHandle, u->passwdLen+1);
+    Mem_CopyFromHandle(&ptr, u->notesHandle, u->notesLen+1);
+
+    return buf;
+}
+
+
+/*
+ * Encrypt and store an unpacked record into an open and locked
+ * database record.
+ */
+void Keys_WriteRecord(UnpackedKeyType const *unpacked, void *recPtr,
+                      UInt8 *cryptKey)
+{
+    UInt32     off = 0;
+    void       *bodyBuf;
+
+    DB_WriteStringFromHandle(recPtr, &off, unpacked->nameHandle,
+                             unpacked->nameLen + 1); /* NUL */
+    bodyBuf = Keys_PackBody(unpacked);
+    DES3_Write(recPtr, off, bodyBuf, packBodyLen, cryptKey);
+    MemPtrFree(bodyBuf);
+}
+
+
+/*
+ * Write just a single NUL to keep space for this record.  It's not
+ * marked dirty yet -- that'll happen when some real data is written
+ * in.
+ */
+Err KeyDB_CreateNew(UInt16 *idx)
+{
+    MemHandle	recHandle;
+    Char        *ptr;
+    Err         err;
+
+    *idx = dmMaxRecordIndex;
+    recHandle = DmNewRecord(gKeyDB, idx, 1);
+    if (!recHandle) 
+        goto findErrOut;
+
+    ptr = MemHandleLock(recHandle);
+    if (!ptr)
+        goto findErrOut;
+    
+    if ((err = DmWrite(ptr, 0, "", 1)))
+        goto errOut;
+    
+    MemHandleUnlock(recHandle);
+    /* XXX: I'm not sure if we should mark the record dirty at this
+     * point or not.  If we do, then it might be sync'd all the way
+     * back to the PC even if it's never used. */
+    if ((err = DmReleaseRecord(gKeyDB, *idx, true)))
+        goto errOut;
+    
+    return 0;
+    
+ findErrOut:
+    err = DmGetLastErr();
+ errOut:
+    UI_ReportSysError2(ID_KeyDatabaseAlert, err, __FUNCTION__);
+    return err;
+}
+
+
+static MemHandle Keys_PrepareExisting(UInt16 *idx, Int16 recLen)
+{
+    MemHandle	recHandle;
+
+    recHandle = DmResizeRecord(gKeyDB, *idx, recLen);
+    if (!recHandle) {
+	UI_ReportSysError2(ID_KeyDatabaseAlert, DmGetLastErr(),
+                           __FUNCTION__);
+	return NULL;
+    }
+
+    return recHandle;
+}
+
+
+void Key_SetCategory(UInt16 idx, UInt16 category)
+{
+    UInt16 attr;
+
+    DmRecordInfo(gKeyDB, idx, &attr, NULL, NULL);
+    attr = (attr & ~dmRecAttrCategoryMask) | category;
+    DmSetRecordInfo(gKeyDB, idx, &attr, NULL);
+}
+
+
+/*
+ * Basic procedure to save a record:
+ *
+ * setup record:
+ *   calculate the required record length
+ *   resize record to the required length
+ * lock record
+ * write plaintext name
+ * write body:
+ *   allocate a temporary buffer equal to the encrypted-part length
+ *   write the unencoded form into the temporary buffer
+ *   allocate a temporary encryption buffer
+ *   one block at a time, encrypt and write into the database
+ *   release temporary buffer
+ * unlock record
+ * set record position
+ */
+void Keys_SaveRecord(UnpackedKeyType const *unpacked, UInt16 *idx)
+{
+    MemHandle recHandle;
+    void	*recPtr;
+    Err		err;
+
+    Keys_CalcPackedSize(unpacked);
+    ErrFatalDisplayIf(packRecLen > 8000,
+		      __FUNCTION__ ": immmoderate packRecLen"); /* paranoia */
+
+    ErrNonFatalDisplayIf(idx == kNoRecord,
+                         __FUNCTION__ ": no record to save");
+    
+    ErrFatalDisplayIf(*idx > kMaxRecords, __FUNCTION__ ": outlandish idx");
+    recHandle = Keys_PrepareExisting(idx, packRecLen);
+    if (!recHandle)
+	return;
+    
+    recPtr = MemHandleLock(recHandle);
+    Keys_WriteRecord(unpacked, recPtr, g_Snib->recordKey);
+    MemHandleUnlock(recHandle);
+
+    err = DmReleaseRecord(gKeyDB, *idx, true);
+    if (err)
+	UI_ReportSysError2(ID_KeyDatabaseAlert, err, __FUNCTION__);
+}
+
