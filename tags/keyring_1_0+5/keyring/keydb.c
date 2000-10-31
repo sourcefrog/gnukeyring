83897e921803e0a706056943bc82d871e0ffa4da
nobody <nobody@80d6c7a2-0132-0410-bcb4-b0b62b5cc441>
2000-10-31 13:36:35 +0000

    This commit was manufactured by cvs2svn to create tag 'keyring_1_0+5'.

---
+++ tags/keyring_1_0+5/keyring/keydb.c
@@ -0,0 +1,410 @@
+/* -*- c-file-style: "k&r"; -*-
+ *
+ * $Id$
+ * 
+ * GNU Keyring -- store passwords securely on a handheld
+ * Copyright (C) 1999, 2000 by Martin Pool <mbp@humbug.org.au>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/* TODO: Check that the terminating NULs in the strings are accounted
+ * for correctly. */
+
+#include <PalmOS.h>
+
+#include "keyring.h"
+#include "keydb.h"
+#include "passwd.h"
+#include "error.h"
+#include "uiutil.h"
+#include "auto.h"
+#include "upgrade.h"
+
+
+Int16 gKeyDBCardNo;
+LocalID gKeyDBID;
+
+// Reference to the keys database
+DmOpenRef       gKeyDB;
+
+/*
+ * True if the database can only be opened read-only; if e.g. the database is
+ * stored in ROM.
+ */
+Boolean g_ReadOnly;
+
+
+/* ======================================================================
+ * Key database
+ *
+ * All the keys are kept in a single PalmOS database.  Each record
+ * begins with an unencrypted name, which is followed by a
+ * 3DES-encrypted block containing all the other fields.
+ *
+ * We encrypt all the records with a key directly derived from the MD5
+ * hash of the master password.  This gives a 128 bit hash.  We split
+ * this into two halves of 64 bits, and use them as DES encryption
+ * keys K1 and K2.  (DES ignores a parity bit from each byte, so there
+ * are actually only 56 unknown bits in each key.)
+ *
+ * As suggested in Schneier's ACv2, each block of the output is
+ * encrypted as ENC(K1,DEC(K2,ENC(K1))).  Since we expect the records
+ * to be relatively short, we don't worry about chaining blocks at the
+ * moment: each is independently encrypted.
+ *
+ * We also need to be able to tell whether the user has entered the
+ * right master password, since we want to give them an error message
+ * rather than just display random garbage.  Therefore a salted hash
+ * of the master password is also stored.  This goes into record #0.
+ *
+ * Rather than worrying about creating the reserved record when it's
+ * used, we create them with the database so we know they'll never be
+ * used.  These records are marked secret, because that flag is not
+ * otherwise used in this application.
+ *
+ * When the user changes their password, we have to walk through the
+ * database, decrypt each record with the old key, and re-encrypt with
+ * the new key.
+ *
+ * NOTE: This scheme is not implemented in the current codebase,
+ * because 0.13 up to dev3 digressed towards using an
+ * independently-generated session key.  But it will be correct for
+ * the final 0.13 release, and is almost correct for previous versions
+ * except that the data was stored in AppInfo or SortInfo rather than
+ * in record 0. */
+
+
+
+
+Int16 Keys_IdxOffsetReserved(void)
+{
+    if (gPrefs.category == 0 || gPrefs.category == dmAllCategories) 
+	return kNumHiddenRecs;
+    else
+	return 0;
+}
+
+
+
+static Boolean KeyDB_OfferReadOnly(void)
+{
+    return FrmAlert(alertID_OfferReadOnly) == 0;	/* button 0 = ok */
+}
+
+
+
+static void Keyring_SetReadOnlyAcceptable(Boolean val)
+{
+     Int16 size = sizeof val;
+     Boolean useSavedPref = true;
+    
+     PrefSetAppPreferences(kKeyringCreatorID,
+                           prefID_ReadOnlyAccepted,
+                           kAppVersion,
+                           &val, size,
+                           useSavedPref);
+}
+
+
+/*
+ * Either check that the user's already said a read-only database is
+ * OK, or ask them.
+ */
+static Boolean Keyring_AcceptsReadOnly(void)
+{
+     Boolean accepted = false;
+     Int16 size = sizeof accepted;
+     Int16 ret;
+     Boolean useSavedPref = true;
+
+     ret = PrefGetAppPreferences(kKeyringCreatorID,
+                                 prefID_ReadOnlyAccepted,
+                                 &accepted, &size,
+                                 useSavedPref);
+
+     if (((ret == noPreferenceFound)
+          || (size != sizeof accepted)
+          || !accepted)) {
+          accepted = KeyDB_OfferReadOnly();
+          if (accepted)
+               Keyring_SetReadOnlyAcceptable(accepted);
+          return accepted;
+     } else {
+          return true;
+     }
+}
+
+
+
+/*
+ * Try to open an existing key database.
+ * 
+ * Will return an error if the database does not exist, in which case
+ * you can try to create a new one.  
+ */
+static Err KeyDB_OpenExistingDB(void) {
+     Err err;
+    
+     // TODO: Give people the option to name the database, or to create
+     // it on different cards?
+     gKeyDB = DmOpenDatabaseByTypeCreator(kKeyDBType, kKeyringCreatorID,
+                                          dmModeReadWrite);
+     if (!gKeyDB)
+          return DmGetLastErr();
+
+     if ((err = DmOpenDatabaseInfo(gKeyDB, &gKeyDBID, NULL, NULL,
+                                   &gKeyDBCardNo, NULL)))
+          return err;
+
+     g_ReadOnly = false;
+
+     return 0;
+}
+
+
+static Err KeyDB_OpenReadOnly(void)
+{
+     Err err;
+    
+     // TODO: Give people the option to name the database, or to create
+     // it on different cards?
+     gKeyDB = DmOpenDatabaseByTypeCreator(kKeyDBType, kKeyringCreatorID,
+                                          dmModeReadOnly);
+     if (!gKeyDB)
+          return DmGetLastErr();
+
+     if ((err = DmOpenDatabaseInfo(gKeyDB, &gKeyDBID, NULL, NULL,
+                                   &gKeyDBCardNo, NULL)))
+          return err;
+
+     g_ReadOnly = true;
+
+     return 0;
+}
+
+
+/*
+ * Create the reserved records that will contain the master password
+ * check-hash.  We don't populate the record yet, but creating it here
+ * means that later on we can just count on these records existing and
+ * being in the right place.
+ *
+ * gKeyDB is open and refers to an empty database when this is called.
+ *
+ * At the moment we use only a single reserved record, but this
+ * function can handle several.
+ */
+Err KeyDB_CreateReservedRecords(void)
+{
+    Err err;
+    Int16 i, idx;
+    UInt16 attr;
+    MemHandle recHandle;
+
+    for (i = 0; i < kNumHiddenRecs; i++) {
+	idx = i;
+	recHandle = DmNewRecord(gKeyDB, &idx, 1);
+	if (!recHandle) {
+	    err = DmGetLastErr();
+	    goto outErr;
+	}
+	ErrNonFatalDisplayIf(idx != i, __FUNCTION__ " inserted into wrong place");
+
+	if ((err = DmReleaseRecord(gKeyDB, idx, true)))
+	    goto outErr;
+
+	attr = dmRecAttrSecret | dmRecAttrDirty;
+	if ((err = DmSetRecordInfo(gKeyDB, idx, &attr, NULL)))
+	    goto outErr;
+    }
+
+    return 0;
+
+ outErr:
+    UI_ReportSysError2(ID_CreateDBAlert, err, __FUNCTION__);
+    return err;
+}
+
+
+Err KeyDB_SetVersion(void) 
+{
+    UInt16 version = kDatabaseVersion;
+    return DmSetDatabaseInfo(gKeyDBCardNo, gKeyDBID,
+			     NULL, NULL,
+			     &version,
+			     NULL, NULL, NULL, NULL,
+			     NULL, NULL, NULL, NULL);
+}
+
+
+/* Set the backup bit.  It seems that without this the Windows desktop
+ * software doesn't make the backup properly */
+static Err KeyDB_MarkForBackup(void) {
+    UInt16 attr;
+    
+    /* TODO: Here or elsewhere set the database version! */
+    
+    DmDatabaseInfo(gKeyDBCardNo, gKeyDBID, NULL, &attr, NULL, NULL,
+		   NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+    attr |= dmHdrAttrBackup;
+    DmSetDatabaseInfo(gKeyDBCardNo, gKeyDBID, NULL, &attr, NULL, NULL,
+		      NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+    return 0;
+}
+
+
+static Err KeyDB_GetVersion(UInt16 *ver) {
+    return DmDatabaseInfo(gKeyDBCardNo, gKeyDBID, 0, 0,
+			  ver,
+			  0, 0, 0, 0,
+			  0, 0, 0, 0);
+}
+
+
+static Err KeyDB_CreateDB(void) {
+    Err err;
+
+    gKeyDBCardNo = 0;
+    if ((err = DmCreateDatabase(gKeyDBCardNo, kKeyDBName,
+				kKeyringCreatorID, kKeyDBType,
+				false /* not resource */)))
+	goto outErr;
+
+    gKeyDBID = DmFindDatabase(gKeyDBCardNo, kKeyDBName);
+    if (!gKeyDBID)
+	goto outFindErr;
+
+    if ((err = KeyDB_SetVersion()))
+	goto outErr;
+
+    gKeyDB = DmOpenDatabase(gKeyDBCardNo, gKeyDBID, dmModeReadWrite);
+    if (!gKeyDB)
+	goto outFindErr;
+
+    if ((err = KeyDB_CreateReservedRecords()))
+	return err;
+    
+    if ((err = KeyDB_CreateCategories()))
+	goto outErr;
+    
+    if (!SetPasswd_Run())
+	return appCancelled;
+
+    if ((err = KeyDB_MarkForBackup()))
+	goto outErr;
+
+    return 0;
+    
+ outFindErr:
+    err = DmGetLastErr();
+    
+ outErr:
+    UI_ReportSysError2(ID_CreateDBAlert, err, __FUNCTION__);
+    return err;
+}
+
+
+
+static Err KeyDB_InitReadOnly(void)
+{
+     Err err;
+     Int16 ver;
+     
+     if (!Keyring_AcceptsReadOnly())
+          return appCancelled;
+
+     if ((err = KeyDB_OpenReadOnly()))
+          return err;
+          
+     if ((err = KeyDB_GetVersion(&ver)))
+          return err;
+          
+     if (ver > kDatabaseVersion) {
+          Keyring_TooNew();
+          return appCancelled;
+     } else if (ver != kDatabaseVersion) {
+          FrmAlert(alertID_UpgradeReadOnly);
+          return appCancelled;
+     }
+
+     return 0;
+}
+
+
+
+/*
+ * Get everything going: either open an existing DB (converting if
+ * necessary), or create a new one, or return an error.
+ */
+Err KeyDB_Init(void)
+{
+     Err                err;
+     UInt16     ver;
+    
+     /* If the database doesn't already exist, then we require the user
+      * to set their password. */
+     err = KeyDB_OpenExistingDB();
+    
+     /* TODO: Check for dmErrReadOnly, dmErrROMBased and offer to open
+      * the database read only.  If so, and the version is old, then
+      * complain that we can't upgrade it. */
+
+     if (err == dmErrReadOnly || err == dmErrROMBased) {
+          if ((err = KeyDB_InitReadOnly())) {
+               if (err == appCancelled)
+                    return err;
+               else
+                    goto failDB;
+          }
+     } else if (err == dmErrCantFind && (err = KeyDB_CreateDB())) {
+          return err;           /* error already reported */
+     } else if (err) {
+          goto failDB;
+     } else {
+          /* So, we opened a database OK.  Now, is it old, new, or just right? */
+          if ((err = KeyDB_GetVersion(&ver)))
+               goto failDB;
+          if (ver < kDatabaseVersion) {
+               if (Keyring_OfferUpgrade()) {
+                    if ((err = UpgradeDB(ver)))
+                         return err;
+
+                    /* We always mark the database here, because we may
+                     * have converted from an old version of keyring that
+                     * didn't do that. */
+                    if ((err = KeyDB_MarkForBackup()))
+                         goto failDB;
+
+               } else {
+                    return 1;
+               }
+          } else if (ver > kDatabaseVersion) {
+               Keyring_TooNew();
+               return appCancelled;
+          }
+
+          /* Next time they get a r/o database they'll have to
+           * reconfirm. */
+          Keyring_SetReadOnlyAcceptable(false);
+     }
+
+     return 0;
+
+ failDB:
+     UI_ReportSysError2(ID_KeyDatabaseAlert, err, __FUNCTION__);
+     return err;
+}
---
+++ tags/keyring_1_0+5/keyring/listform.c
@@ -0,0 +1,421 @@
+/* -*- c-file-style: "k&r"; -*-
+ *
+ * $Id$
+ * 
+ * GNU Keyring -- store passwords securely on a handheld
+ * Copyright (C) 1999, 2000 Martin Pool <mbp@humbug.org.au>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <PalmOS.h>
+#include <Password.h>
+#include <Encrypt.h>
+
+#include "resource.h"
+#include "keyring.h"
+#include "uiutil.h"
+#include "passwd.h"
+#include "category.h"
+#include "listform.h"
+#include "keydb.h"
+#include "auto.h"
+#include "keyedit.h"
+
+// =====================================================================
+// List form
+
+/*
+ * TODO: As a possible optimization, scroll the bitmap of the table
+ * rather than redrawing it.  See ListViewScroll in the MemoPad
+ * source.
+ *
+ * TODO: Show category headings in the list.
+ *
+ * TODO: Scroll to show newly-inserted items.
+ */
+
+static TablePtr f_Table;
+static ScrollBarPtr f_ScrollBar;
+static FormPtr f_ListForm;
+
+/* Number of table rows that could possibly fit on the screen.  Some
+ * of them might not be actually in use if there's less than a screen
+ * of records. */
+static Int16 f_ScreenRows;
+
+/* Pixel width of the table */
+static Int16 f_TableWidth, f_TableHeight;
+
+/* Number of records available to be shown in the table, after taking
+ * into account categories and reserved records. */
+static Int16 f_NumListed;
+
+/* Index of the first record displayed in the table.  Zero shows top
+ * of table, etc. */
+Int16 f_FirstIdx;
+
+/* Width of '.' character in pixels */
+static Int16 f_DotWidth;
+
+static Int16 ListForm_RecordIdx(Int16 itemNum)
+{
+    Int16       idx = 0;
+    Err         err;
+
+    /* FIXME: We need to skip the first few reserved records, but that
+     * depends on the category: if it is zero (if that's their
+     * category) or all, then we must increase the itemNum; otherwise
+     * not.
+     *
+     * Also, we need to selectively reduce the number of records in
+     * the list by that amount, and also compensate when mapping list position
+     * to record idx.
+     *
+     * Is there no "hidden" bit we can set to avoid all this?
+     */
+    
+    err = DmSeekRecordInCategory(gKeyDB, &idx, itemNum,
+                                 dmSeekForward, gPrefs.category);
+
+    if (err != errNone) {
+        return -1;
+    } 
+
+    return idx;
+}
+
+
+static void ListForm_DrawToFit(Char const * name, Int16 x, Int16 y)
+{
+    Int16 titleLen, width, titleWidth;
+    Int16 charsToDraw;
+    Boolean stringFit;
+
+    charsToDraw = StrLen(name);
+
+    width = f_TableWidth - x;
+    titleWidth = width;
+    titleLen = charsToDraw;
+    FntCharsInWidth(name, &titleWidth, &titleLen, &stringFit);
+
+    if (stringFit) {
+        WinDrawChars(name, titleLen, x, y);
+    } else {
+        width -= (f_DotWidth * 3);
+        while (titleWidth > width || 
+               name[titleLen - 1] == ' ' || 
+               name[titleLen - 1] == tabChr)
+            {
+                titleWidth -= FntCharWidth(name[--titleLen]);
+            }
+        WinDrawChars(name, titleLen, x, y);
+        x += titleWidth;
+        WinDrawChars("...", 3, x, y);
+    }
+}
+
+
+static void ListForm_DrawCell(TablePtr UNUSED(table),
+                              Int16 row, Int16 UNUSED(col), 
+                              RectanglePtr bounds)
+{
+    /* We keep deleted records at the end, so the first N records will
+     * be ones we can draw. */
+
+    /* This could be faster if we remembered the current position and
+     * stepped forward to the next.  However it's not worth optimizing
+     * since it will likely change to be a table in the future. */
+    MemHandle   rec = 0;
+    Char const *recPtr = 0, *scrStr;
+    Char        altBuf[10];
+    Int16       idx = row;
+
+    ErrFatalDisplayIf(row > 10000, __FUNCTION__ ": unreasonable itemnum");
+
+    idx = ListForm_RecordIdx(row + f_FirstIdx); 
+    if (idx == -1) { 
+        scrStr = "<err>"; 
+         goto draw; 
+      } 
+
+    rec = DmQueryRecord(gKeyDB, idx);
+    if (!rec) {
+        scrStr = "<no-record>";
+        goto draw;
+    }
+    
+    recPtr = MemHandleLock(rec); 
+    if (!recPtr) {
+        scrStr = "<no-ptr>";
+        goto draw;
+    }
+    
+    if (!*recPtr) {
+        /* If there is no name, use the record index instead.  At the
+         * moment this skips over records that have been deleted, but
+         * I think once we've included sorting that will not
+         * happen. */
+        altBuf[0] = '#';
+        StrIToA(altBuf + 1, idx - kNumHiddenRecs);
+        scrStr = altBuf;
+    } else {
+        scrStr = recPtr;
+    }
+
+ draw:
+    /* TODO: Maybe add ellipsis if too wide? */
+    ListForm_DrawToFit(scrStr, bounds->topLeft.x + 2, bounds->topLeft.y);
+    
+    if (recPtr)
+        MemHandleUnlock(rec);
+}
+
+
+/*
+ * Update the table control, after querying the database to see how
+ * many rows can be displayed.
+ */
+static void ListForm_UpdateTable(void)
+{
+    Int16 row;
+    Int16 lineHeight;
+    Int16 dataHeight;
+    Int16 maxRows;
+
+    lineHeight = FntLineHeight();
+
+    /* Get the total number of rows allocated.  */
+    maxRows = TblGetNumberOfRows(f_Table);
+
+    f_NumListed = DmNumRecordsInCategory(gKeyDB, gPrefs.category);
+    ErrNonFatalDisplayIf(f_NumListed > 30000,
+                         "unreasonable numListed");
+
+    f_ScreenRows = f_TableHeight / lineHeight;
+
+    /* Work out which record should be at the top.  It can't be less
+     * than zero of course, but also we don't allow whitespace at the
+     * bottom if there are enough rows to fill the display.  However
+     * we leave the display as close as possible to where the user put
+     * it last. */
+    if (f_FirstIdx > f_NumListed - f_ScreenRows)
+         f_FirstIdx = f_NumListed - f_ScreenRows;
+    if (f_FirstIdx < 0)
+         f_FirstIdx = 0;
+
+    /* Update all row controls, but also mark ones for which there is
+     * no data as not usable.
+     *
+     * TODO: Try to avoid calling all the TblSet routines more than
+     * once per invocation of the list form. */
+    dataHeight = 0;
+    for (row = 0; row < maxRows; row++) {
+        if ((f_TableHeight >= dataHeight + lineHeight)
+            && (row + f_FirstIdx < f_NumListed)) {
+            /* Row is usable */
+            TblSetRowHeight(f_Table, row, lineHeight);
+            TblSetItemStyle(f_Table, row, 0, customTableItem);
+            TblSetRowUsable(f_Table, row, true);
+        } else {
+            TblSetRowUsable(f_Table, row, false);
+        }
+        dataHeight += lineHeight;
+    }
+    TblSetColumnUsable(f_Table, 0, true);
+    
+    TblSetCustomDrawProcedure(f_Table, 0,
+                              (TableDrawItemFuncPtr) ListForm_DrawCell);
+
+    TblHasScrollBar(f_Table, true);
+}
+
+
+static void ListForm_UpdateScrollBar(void)
+{
+    /*
+     * Connect the scrollbar to the list.  The list cannot change
+     * while this form is displayed: insertion, deletion, and renaming
+     * all happen from inside the key details form.  Therefore, we
+     * don't have to update the size of the scroll bar.
+     */
+    Int16 max;
+
+    max = f_NumListed - f_ScreenRows;
+    if (max < 0) {
+        /* Less than one page of records. */
+        max = 0;
+    }
+    SclSetScrollBar(f_ScrollBar, f_FirstIdx, 0, max, f_ScreenRows);
+}
+
+
+
+
+static void ListForm_UpdateCategory(void)
+{
+    Category_UpdateName(f_ListForm, gPrefs.category);
+}
+
+
+
+static void ListForm_Update(void)
+{
+    ListForm_UpdateTable();
+    ListForm_UpdateCategory();
+    ListForm_UpdateScrollBar();
+    
+    FrmDrawForm(f_ListForm);
+}
+
+
+static void ListForm_FormOpen(void) {
+    RectangleType r;
+       
+    f_ListForm = FrmGetActiveForm();
+    f_Table = UI_GetObjectByID(f_ListForm, ID_KeyTable);
+    f_ScrollBar = UI_GetObjectByID(f_ListForm, ID_KeyTableScrollBar);
+
+    TblGetBounds(f_Table, &r);
+    f_TableHeight = r.extent.y;
+    f_TableWidth = r.extent.x;
+
+    f_DotWidth = FntCharWidth('.');
+
+    ListForm_Update();
+}
+
+
+static Boolean ListForm_TableSelect(EventPtr event)
+{
+    Int16       listIdx, idx;
+    Err         err;
+    if (event->data.tblSelect.tableID != ID_KeyTable)
+        return false;
+
+    if (Unlock_CheckTimeout() || UnlockForm_Run()) {
+        /* Map from a position within this category to an overall
+         * record index. */
+        
+        listIdx = f_FirstIdx + event->data.tblSelect.row;
+        idx = 0;
+        err = DmSeekRecordInCategory(gKeyDB, &idx, listIdx,
+                                     dmSeekForward, gPrefs.category);
+        KeyEditForm_GotoRecord(idx);
+        FrmGotoForm(KeyEditForm);
+    }
+    return true;
+}
+
+
+static void ListForm_NewKey(void) {
+     if (Unlock_CheckTimeout() || UnlockForm_Run()) {
+          KeyEditForm_GotoNew();
+     }
+}
+
+
+/*
+ * Scroll if possible.  Update table and scrollbar.
+ */
+static void ListForm_Scroll(Int16 newPos) {
+     if (newPos > f_NumListed - f_ScreenRows)
+          newPos = f_NumListed - f_ScreenRows;
+     if (newPos < 0)
+          newPos = 0;
+     
+     f_FirstIdx = newPos;
+
+     ListForm_UpdateScrollBar();
+     TblMarkTableInvalid(f_Table);
+     TblRedrawTable(f_Table);
+}
+
+
+static void ListForm_ScrollRepeat(EventPtr event) {
+    int new = event->data.sclRepeat.newValue;
+    ListForm_Scroll(new);
+}
+
+
+static void ListForm_ScrollPage(WinDirectionType dir) {
+    Int16 newPos = f_FirstIdx +
+        ((dir == winDown) ? +f_ScreenRows : -f_ScreenRows);
+    ListForm_Scroll(newPos);
+}
+
+
+static void ListForm_CategoryTrigger(void)
+{
+    if (Category_Selected(&gPrefs.category, true)) {
+        /* XXX: Is this really the only place we have to erase? */
+        TblEraseTable(f_Table);
+        ListForm_Update();
+    }
+}
+
+
+Boolean ListForm_HandleEvent(EventPtr event) {
+    Boolean result = false;
+    
+    switch (event->eType) {
+    case ctlSelectEvent:
+        switch (event->data.ctlSelect.controlID) {
+        case NewKeyBtn:
+            ListForm_NewKey();
+            result = true;
+            break;
+
+        case CategoryTrigger:
+            ListForm_CategoryTrigger();
+            return true;
+        }
+        break;
+
+    case frmOpenEvent:
+        ListForm_FormOpen();
+        return true;
+
+    case menuEvent:
+        if (!Common_HandleMenuEvent(event))
+            App_NotImplemented();
+        result = true;
+        break;
+
+    case sclRepeatEvent:
+        ListForm_ScrollRepeat(event);
+        break;
+
+    case tblSelectEvent:
+        return ListForm_TableSelect(event);
+
+    case keyDownEvent:
+        if (event->data.keyDown.chr == pageUpChr) {
+            ListForm_ScrollPage(winUp);
+            return true;
+        } else if (event->data.keyDown.chr == pageDownChr) {
+            ListForm_ScrollPage(winDown);
+            return true;
+        }
+        break;
+
+    default:
+        ;       
+    }
+
+    return result;
+}
+
+
