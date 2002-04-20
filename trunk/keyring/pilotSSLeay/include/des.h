ffeef9e0d6e81c739bf50ba5b108f4c006e2b42d
hoenicke <hoenicke@80d6c7a2-0132-0410-bcb4-b0b62b5cc441>
2002-04-20 14:48:12 +0000

    Fixed warnings in pilotSSL stuff. Updated Makefile dist rules.

--- trunk/keyring/pilotSSLeay/include/des.h
+++ trunk/keyring/pilotSSLeay/include/des.h
@@ -215,7 +215,7 @@ char *des_crypt(const char *buf,const char *salt);
 #ifdef HEADER_DES_LOCL_H
 char *crypt(const char *buf,const char *salt);
 #else
-char *crypt();
+char *crypt(void);
 #endif
 #endif
 void des_ofb_encrypt(unsigned char *in,unsigned char *out,
