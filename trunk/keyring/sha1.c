e2355a9382c3b8e874f4d1cfcb11c4cc84b6dbea
hoenicke <hoenicke@80d6c7a2-0132-0410-bcb4-b0b62b5cc441>
2003-11-09 13:35:21 +0000

    Simplified SHA1 code

--- trunk/keyring/sha1.c
+++ trunk/keyring/sha1.c
@@ -25,15 +25,36 @@
 #define WORDS_BIGENDIAN 1
 
 #ifdef WORDS_BIGENDIAN
-# define SWAP(n) (n)
+#define MemMoveSwap(d,s,n) MemMove(d,s,n)
 #else
-# define SWAP(n)							\
-    (((n) << 24) | (((n) & 0xff00) << 8) | (((n) >> 8) & 0xff00) | ((n) >> 24))
+static void
+MemMoveSwap (void *dest, const void *src, unsigned int len)
+{
+    char *d = dest;
+    char *s = src;
+    while (len >= 4) {
+	d[3] = s[0]; d[2] = s[1]; d[1] = s[2]; d[0] = s[3];
+	d += 4;
+	s += 4;
+	len -= 4;
+    }
+    switch (len) {
+    case 3:
+	d[1] = s[2]; 
+    case 2:
+	d[2] = s[1]; 
+    case 1:
+	d[3] = s[0];
+    case 0:
+    }
+}
 #endif
 
 /* SHA initial values */
-
-const UInt32 initsha1[5] =  {
+/* This would be a const array, but unfortunately prc-tools just
+ * produces illegal code.
+ */
+UInt32 initsha1[5] =  {
     0x67452301L,
     0xEFCDAB89L,
     0x98BADCFEL,
@@ -132,122 +153,60 @@ SHA1_Block_ (const UInt32 *digin, UInt32 *buffer, UInt32 *digout)
 }
 #endif
 
-SHA1_SECTION void
-SHA1_Init (SHA_CTX *ctx)
+#if 0 /* This function is no longer used */
+
+/* Compute SHA1 message digest for LEN bytes beginning at BUFFER.  The
+   result is always in big endian byte order, so that a byte-wise
+   output yields to the wanted ASCII representation of the message
+   digest.  */
+void 
+SHA1(const unsigned char *buffer, unsigned long len, unsigned char *resblock)
 {
-    MemMove(ctx->dig, initsha1, sizeof(initsha1));
-    ctx->len = ctx->lenHi = 0;
-}
-
-/* Process the remaining bytes in the internal buffer and the usual
-   prolog according to the standard and write the result to RESBUF.
-
-   IMPORTANT: On some systems it is required that RESBUF is correctly
-   aligned for a 32 bits value.  */
-SHA1_SECTION void
-SHA1_Final (unsigned char *resbuf, SHA_CTX *ctx)
-{
-  /* Take yet unprocessed bytes into account.  */
-  UInt16 bytes = ctx->len & (kSHA1BlockSize - 1);
-  UInt32 *data;
-  Char   *cdata;
-#ifndef WORDS_BIGENDIAN
-  int i;
-#endif
-
-  /* Now count remaining bytes.  */
-
-  cdata = (char*) ctx->data;
-  cdata[bytes++] = 0x80;
-  if (bytes > 56) {
-      MemSet(cdata + bytes, 64 - bytes, 0);
-      data = ctx->data;
-#ifndef WORDS_BIGENDIAN
-      for (i = 0; i < 16; i++)
-	  *data++ = SWAP(*data);
-#endif
-      SHA1_Block(ctx->dig, ctx->data, ctx->dig);
-      MemSet(cdata, 56, 0);
-  } else {
-      MemSet(cdata + bytes, 56 - bytes, 0);
-  }
-
-  data = ctx->data;
-#ifndef WORDS_BIGENDIAN
-  for (i = 0; i < 14; i++)
-      *data++ = SWAP(*data);
-#endif
-
-  /* Put the 64-bit file length in *bits* at the end of the buffer.  */
-  ctx->data[14] = (ctx->lenHi << 3) | (ctx->len >> 29);
-  ctx->data[15] = ctx->len << 3;
-
-  /* Process last bytes.  */
-  SHA1_Block (ctx->dig, ctx->data, ctx->dig);
-
-#ifndef WORDS_BIGENDIAN
-  for (i = 0; i < 4; i++)
-      ctx->dig[i] = SWAP (ctx->dig[i]);
-#endif
-
-  MemMove(resbuf, ctx->dig, sizeof(ctx->dig));
-  /* Erase private data */
-  MemSet(ctx, sizeof(ctx), 0);
-}
-
-SHA1_SECTION void
-SHA1_Update (SHA_CTX *ctx, const unsigned char *buffer, unsigned long len)
-{
-    int i;
-    UInt16 left_over = ctx->len & (kSHA1BlockSize - 1);
-    UInt16 add = 64 - left_over;
-    UInt32 *data;
-
-    ctx->len += len;
-    if (ctx->len < len)
-	ctx->lenHi++;
+    struct {
+	UInt32 data[16];
+	UInt32 dig[5];
+    } b;
+    UInt32 remaining = len;
+    const UInt32 *digp = initsha1;
 
     /* When we already have some bits in our internal buffer concatenate
        both inputs first.  */
-    while (len >= add) {
-	data = ctx->data;
-	MemMove (((char*)data) + left_over, buffer, add);
-
-	for (i = 0; i < 16; i++)
-	    *data++ = SWAP(*data);
-	SHA1_Block (ctx->dig, ctx->data, ctx->dig);
-	buffer   += add;
-	len      -= add;
-	left_over = 0;
-	add       = 64;
+    while (remaining >= kSHA1BlockSize) {
+	MemMoveSwap (b.data, buffer, kSHA1BlockSize);
+	SHA1_Block (digp, b.data, b.dig);
+	buffer    += kSHA1BlockSize;
+	remaining -= kSHA1BlockSize;
+	digp = b.dig;
     }
     
     /* Move remaining bytes in internal buffer.  */
-    if (len > 0)
-	MemMove (((char*)ctx->data) + left_over, buffer, len);
+    MemSet(b.data, sizeof(b.data), 0);
+    MemMoveSwap (b.data, buffer, remaining);
+#ifdef WORDS_BIGENDIAN
+    ((char*) b.data)[remaining] = 0x80;
+#else
+    ((char*) b.data)[remaining ^ 3] = 0x80;
+#endif
+
+    if ((len & 63) > 55) {
+	SHA1_Block(digp, b.data, b.dig);
+	digp = b.dig;
+	MemSet(b.data, sizeof(b.data), 0);
+    }
+
+    /* Put the 64-bit file length in *bits* at the end of the buffer.  */
+    /* We only put 32-bits as we are never called with 512MB blocks */
+    b.data[15] = len << 3;
+    
+    /* Process last bytes.  */
+    SHA1_Block (digp, b.data, b.dig);
+    
+    MemMoveSwap(resblock, b.dig, sizeof(b.dig));
+
+    /* Erase private data */
+    MemSet(&b, sizeof(b), 0);
 }
-
-/* Compute SHA1 message digest for LEN bytes beginning at BUFFER.  The
-   result is always in little endian byte order, so that a byte-wise
-   output yields to the wanted ASCII representation of the message
-   digest.  */
-SHA1_SECTION unsigned char *
-SHA1(const unsigned char *buffer, unsigned long len, unsigned char *resblock)
-{
-  SHA_CTX ctx;
-
-  /* Initialize the computation context.  */
-  SHA1_Init (&ctx);
-
-  /* Process whole buffer but last len % 64 bytes.  */
-  SHA1_Update (&ctx, buffer, len);
-
-  /* Put result in desired memory area.  */
-  SHA1_Final (resblock, &ctx);
-
-  return resblock;
-}
-
+#endif
 
 SHA1_SECTION
 void PwHash_PBKDF2(void* result, int resultLen, 
@@ -265,14 +224,13 @@ void PwHash_PBKDF2(void* result, int resultLen,
     /* Prepare the HMAC buffers */
     MemSet(b.block, sizeof(b.block), 0);
 
-    if (StrLen(passwd) > kSHA1BlockSize)
-	SHA1(passwd, StrLen(passwd), (Char *) b.block);
-    else
-	MemMove(b.block, passwd, StrLen(passwd));
+    ErrNonFatalDisplayIf(StrLen(passwd) > kSHA1BlockSize,
+			 __FUNCTION__ " password too long");
+    MemMoveSwap(b.block, passwd, StrLen(passwd));
 
     data = b.block;
     for (i = 0; i < 16; i++)
-	*data++ = SWAP(*data) ^ 0x36363636;
+	*data++ ^= 0x36363636;
     SHA1_Block(initsha1, b.block, b.idig);
 
     /* Now prepare opad */
@@ -283,18 +241,18 @@ void PwHash_PBKDF2(void* result, int resultLen,
 
     for (k = 1; resultLen > 0; k++) {
 	MemSet(b.block, sizeof(b.block), 0);
-	MemMove(b.block, salt, kSaltSize);
+	MemMoveSwap(b.block, salt, kSaltSize);
 	b.block[kSaltSize/4] = k;
-	((unsigned char*) b.block)[kSaltSize + 4] = 0x80;
-	b.block[15] = (sizeof(b.block) + kSaltSize + 4) * 8;
+	b.block[kSaltSize/4 + 1] = 0x80000000;
+	b.block[15] = (kSHA1BlockSize + kSaltSize + 4) * 8;
 	SHA1_Block(b.idig, b.block, b.block);
 
 #if kSaltSize + 4 > kSHA1HashSize
 	MemSet(b.block + (kSHA1HashSize / 4), 
 	       kSHA1BlockSize - kSHA1HashSize, 0);
 #endif
-	((unsigned char*) b.block)[kSHA1HashSize] = 0x80;
-	b.block[15] = (sizeof(b.block) + sizeof(b.dig)) * 8;
+	b.block[kSHA1HashSize / 4] = 0x80000000;
+	b.block[15] = (kSHA1BlockSize + kSHA1HashSize) * 8;
 	SHA1_Block(b.odig, b.block, b.block);
 	MemMove(b.dig, b.block, sizeof(b.dig));
 
@@ -308,12 +266,8 @@ void PwHash_PBKDF2(void* result, int resultLen,
 	    b.dig[3] ^= b.block[3];
 	    b.dig[4] ^= b.block[4];
 	}
-#ifndef WORDS_BIGENDIAN
-	for (i = 0; i < 5; i++)
-	    b.dig[i] = SWAP(b.dig[i]);
-#endif
-	MemMove(result, b.dig, 
-		kSHA1HashSize < resultLen ? kSHA1HashSize : resultLen);
+	MemMoveSwap(result, b.dig, 
+		    kSHA1HashSize < resultLen ? kSHA1HashSize : resultLen);
 	result    += kSHA1HashSize;
 	resultLen -= kSHA1HashSize;
     }
@@ -321,4 +275,3 @@ void PwHash_PBKDF2(void* result, int resultLen,
     /* Now erase all buffers, they may contain passwords */
     MemSet(&b, sizeof(b), 0);
 }
-
