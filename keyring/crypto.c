81ed66d0271fa3e1eaefbb0c6ef34e9a4c3cdfaf
Jochen Hoenicke <897826+jhoenicke@users.noreply.github.com>
2005-02-24 22:36:04 +0000

    Updated copyright years

--- keyring/crypto.c
+++ keyring/crypto.c
@@ -4,7 +4,7 @@
  * 
  * Keyring -- store passwords securely on a handheld
  * Copyright (C) 1999, 2000, 2001 Martin Pool <mbp@users.sourceforge.net>
- * Copyright (C) 2001-2003 Jochen Hoenicke <hoenicke@users.sourceforge.net>
+ * Copyright (C) 2001-2005 Jochen Hoenicke <hoenicke@users.sourceforge.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- keyring/resource.h
+++ keyring/resource.h
@@ -1,11 +1,24 @@
 /* -*- mode: c; c-indentation-style: "java"; c-basic-offset: 4 -*-
  *
- * Copyright (C) 1999, 2000 by Martin Pool <mbp@users.sourceforge.net>
- * Copyright (C) 2002-2003 Jochen Hoenicke <hoenicke@users.sourceforge.net>
- *
  * $Header$
+ * 
+ * Keyring -- store passwords securely on a handheld
+ * Copyright (C) 1999, 2000 by Martin Pool <mbp@users.sourceforge.net>
+ * Copyright (C) 2002-2005 Jochen Hoenicke <hoenicke@users.sourceforge.net>
  *
- * Keyring for PalmOS
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #define fntStar				 128
