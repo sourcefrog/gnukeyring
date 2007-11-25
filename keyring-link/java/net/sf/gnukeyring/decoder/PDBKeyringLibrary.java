82fe05d64e11af7cf5d32ade382466e9b99738aa
Jochen Hoenicke <897826+jhoenicke@users.noreply.github.com>
2007-11-25 18:28:21 +0000

    Keyring-2 support

--- keyring-link/java/net/sf/gnukeyring/decoder/PDBKeyringLibrary.java
+++ keyring-link/java/net/sf/gnukeyring/decoder/PDBKeyringLibrary.java
@@ -35,28 +35,65 @@ public class PDBKeyringLibrary implements KeyringLibrary {
     Database db;
     TreeMap categories;
     Cipher cipher;
+    SecretKey key;
+    int    iter;
+    int    ciphnr;
     byte[] salt;
     byte[] mdigest;
     List entries;
+
+    String charSetName = "ISO-8859-1";
 
     static {
 	/* We want SunJCE provider for DES encryption */
 	Security.addProvider(new com.sun.crypto.provider.SunJCE());
     }
 
+    private static int keylens[] = { 8, 24, 16, 32 };
+    private static String keyalgos[] = { "", 
+					 "DESede/CBC/NoPadding",
+					 "AES/CBC/NoPadding",
+					 "AES/CBC/NoPadding" };
+    private static String keyalgosShort[] = { "", 
+					      "DES",
+					      "AES",
+					      "AES" };
+
 
     public static class KeyAppInfo extends AppInfoBlock {
+	byte[] salt;
+	byte[] mdigest;
+	byte[] ivec;
+	int    iter;
+	int    cipher;
+	
 	public void read(PalmDataInputStream in) throws IOException {
 	    readCategories(in);
+	    if (getDatabase().getVersion() >= 5) {
+		salt = new byte[8];
+		mdigest = new byte[8];
+		in.readFully(salt, 0, 8);
+		iter = in.readUnsignedShort();
+		cipher = in.readUnsignedShort();
+		in.readFully(mdigest, 0, 8);
+	    }
 	}
 
 	public void write(PalmDataOutputStream out) throws IOException {
 	    writeCategories(out);
+	    if (getDatabase().getVersion() >= 5) {
+		salt = new byte[8];
+		mdigest = new byte[8];
+		out.write(salt);
+		out.writeShort(iter);
+		out.writeShort(cipher);
+		out.write(mdigest);
+	    }
 	}
     }
 
     public static class KeyRecord extends PDBRecord {
-	String keyname;
+	byte[] keyname;
 	byte[] crypted;
 	byte[] salt;
 	byte[] mdigest;
@@ -69,17 +106,47 @@ public class PDBKeyringLibrary implements KeyringLibrary {
 	    if ((getAttributes() & DMRECATTRDELETE) != 0) {
 		return;
 	    } else if ((getAttributes() & DMRECATTRSECRET) == 0) {
-		byte[] data = new byte[1024];
-		keyname = in.readPackedString();
-		int len = 0;
-		while (len < 1024) {
-		    int read = in.read(data, len, 1024 - len);
-		    if (read <= 0)
-			break;
-		    len += read;
+		if (getDatabase().getVersion() <= 4) {
+		    byte[] data = new byte[1024];
+		    int len = 0;
+		    while (len < 1024) {
+			int read = in.read(data, len, 1024 - len);
+			if (read <= 0)
+			    break;
+			len += read;
+		    }
+		    int idx;
+		    for (idx = 0; idx < len; idx++) {
+			if (data[idx] == 0)
+			    break;
+		    }
+		    if (idx > len)
+			throw new IOException("Keyname not null terminated");
+		    keyname = new byte[idx];
+		    System.arraycopy(data, 0, keyname, 0, idx);
+		    idx++;
+		    crypted = new byte[len - idx];
+		    System.arraycopy(data, idx, crypted, 0, len-idx);
+		} else {
+		    int namelen = in.readUnsignedShort();
+		    in.read();  // field id
+		    in.read();  // font
+		    keyname = new byte[namelen];
+		    in.readFully(keyname, 0, namelen);
+
+		    if ((namelen & 1) == 1)
+			in.read(); // padding;
+		    byte[] data = new byte[1024];
+		    int len = 0;
+		    while (len < 1024) {
+			int read = in.read(data, len, 1024 - len);
+			if (read <= 0)
+			    break;
+			len += read;
+		    }
+		    crypted = new byte[len];
+		    System.arraycopy(data, 0, crypted, 0, len);
 		}
-		crypted = new byte[len];
-		System.arraycopy(data, 0, crypted, 0, len);
 	    } else {
 		salt = new byte[4];
 		mdigest = new byte[16];
@@ -89,11 +156,19 @@ public class PDBKeyringLibrary implements KeyringLibrary {
 	}	
     }
 
+    public void setCharset (String charset) {
+	charSetName = charset;
+    }
+
     public void setFilename (File file) {
 	db = new Database();
 	try {
 	    db.read(new FileInputStream(file), 
 		    KeyAppInfo.class, null, KeyRecord.class);
+	    if (db.getVersion() < 4 || db.getVersion() > 5) {
+		System.err.println("Unsupported database version!");
+		return;
+	    }
 	} catch (Exception ex) {
 	    ex.printStackTrace();
 	    return;
@@ -105,23 +180,46 @@ public class PDBKeyringLibrary implements KeyringLibrary {
 	    if (cat.length() > 0)
 		categories.put(cat, new Integer(i));
 	}
-	    
-	Record[] rawEntries = db.getEntries();
-	salt = ((KeyRecord) rawEntries[0]).salt;
-	mdigest = ((KeyRecord) rawEntries[0]).mdigest;
 
-	entries = new ArrayList(rawEntries.length - 1);
-	for (int i = 1; i < rawEntries.length; i++) {
+	Record[] rawEntries = db.getEntries();
+	int start = 0;
+	if (db.getVersion() <= 4) {
+	    salt = ((KeyRecord) rawEntries[0]).salt;
+	    mdigest = ((KeyRecord) rawEntries[0]).mdigest;
+	    start++;
+	} else {
+	    salt = ((KeyAppInfo) db.getAppInfo()).salt;
+	    ciphnr = ((KeyAppInfo) db.getAppInfo()).cipher;
+	    iter = ((KeyAppInfo) db.getAppInfo()).iter;
+	    mdigest = ((KeyAppInfo) db.getAppInfo()).mdigest;
+	}
+	
+	entries = new ArrayList(rawEntries.length);
+	for (int i = start; i < rawEntries.length; i++) {
 	    KeyRecord entry = (KeyRecord) rawEntries[i];
 	    if ((entry.getAttributes() & entry.DMRECATTRDELETE) != 0)
 		continue;
 	    int cat = (entry.getAttributes() & entry.DMRECATTRCATEGORYMASK);
-	    entries.add(new PDBKeyringEntry(this, entry.keyname, 
-					    db.getAppInfo().getCategories(cat),
-					    entry.crypted));
+	    String name;
+	    try {
+		name = new String(entry.keyname, charSetName);
+	    } catch (UnsupportedEncodingException ex) {
+		ex.printStackTrace();
+		return;
+	    }
+
+	    if (db.getVersion() <= 4) {
+		entries.add(new PDBKeyringEntry(this, name, 
+						db.getAppInfo().getCategories(cat),
+						entry.crypted));
+	    } else {
+		entries.add(new PDBKeyringEntry5(this, name, 
+						 db.getAppInfo().getCategories(cat),
+						 entry.crypted));
+	    }
 	}
     }
-
+	
     public Set getCategories() {
 	return categories.keySet();
     }
@@ -165,36 +263,38 @@ public class PDBKeyringLibrary implements KeyringLibrary {
 
     public boolean unlock(String password)
     {
-	byte[] buff = (new String(salt) + password).getBytes();
-	byte[] pw = new byte[64];
-	System.arraycopy(buff, 0, pw, 0, Math.min(64,buff.length));
-	pw = md5(pw);
-	for (int i = 0; i < 16; i++) {
-	    if (pw[i] != mdigest[i])
-		return false;
-	}
-
-	try {
-	    cipher = Cipher.getInstance("DESede/ECB/NoPadding");
-	} catch (GeneralSecurityException ex) {
-	    throw new InternalError("Can't get DESede encryption: "
-				    +ex.toString());
-	}
-
-	SecretKey key = getKey(password);
-	try {
-	    cipher.init(cipher.DECRYPT_MODE, key);
-	} catch (InvalidKeyException ex) {
-	    throw new InternalError("Invalid key: "+ex);
-	}
-	return true;
+	return getKey(password);
     }
 
     public void lock() {
 	cipher = null;
+	key = null;
     }
 
-    Cipher getCipher() {
+    int getBlockSize() {
+	return cipher.getBlockSize();
+    }
+
+    Cipher getCipher(int mode, byte[] iv) {
+	AlgorithmParameters params = null;
+	try {
+	    if (db.getVersion() > 4) {
+		switch (ciphnr) {
+		    case 1:
+			params = AlgorithmParameters.getInstance("DES");
+			params.init(new IvParameterSpec(iv));
+			break;
+		    case 2:
+		    case 3:
+			params = AlgorithmParameters.getInstance("AES");
+			params.init(new IvParameterSpec(iv));
+			break;
+		}
+	    }
+	    cipher.init(mode, key, params);
+	} catch (GeneralSecurityException ex) {
+	    throw new InternalError("Cannot create IV: "+ex);
+	}
 	return cipher;
     }
 
@@ -202,43 +302,164 @@ public class PDBKeyringLibrary implements KeyringLibrary {
 	return cipher == null;
     }
 
-    private short odd_parity[] = {
-	1,  1,  2,  2,  4,  4,  7,  7,  8,  8, 11, 11, 13, 13, 14, 14,
-	16, 16, 19, 19, 21, 21, 22, 22, 25, 25, 26, 26, 28, 28, 31, 31,
-	32, 32, 35, 35, 37, 37, 38, 38, 41, 41, 42, 42, 44, 44, 47, 47,
-	49, 49, 50, 50, 52, 52, 55, 55, 56, 56, 59, 59, 61, 61, 62, 62,
-	64, 64, 67, 67, 69, 69, 70, 70, 73, 73, 74, 74, 76, 76, 79, 79,
-	81, 81, 82, 82, 84, 84, 87, 87, 88, 88, 91, 91, 93, 93, 94, 94,
-	97, 97, 98, 98,100,100,103,103,104,104,107,107,109,109,110,110,
-	112,112,115,115,117,117,118,118,121,121,122,122,124,124,127,127,
-	128,128,131,131,133,133,134,134,137,137,138,138,140,140,143,143,
-	145,145,146,146,148,148,151,151,152,152,155,155,157,157,158,158,
-	161,161,162,162,164,164,167,167,168,168,171,171,173,173,174,174,
-	176,176,179,179,181,181,182,182,185,185,186,186,188,188,191,191,
-	193,193,194,194,196,196,199,199,200,200,203,203,205,205,206,206,
-	208,208,211,211,213,213,214,214,217,217,218,218,220,220,223,223,
-	224,224,227,227,229,229,230,230,233,233,234,234,236,236,239,239,
-	241,241,242,242,244,244,247,247,248,248,251,251,253,253,254,254
-    };
+    private static byte odd_parity[] = new byte[256];
 
-    public SecretKey getKey(String password) {
-	byte[] digest = md5(password.getBytes());
-	byte[] key = new byte[24];
-	for (int i = 0; i < 16; i++) {
-	    key[i] = (byte) odd_parity[digest[i] & 0xff];
-	}
-	for (int i = 0; i < 8; i++) {
-	    key[i+16] = key[i];
-	}
-	try {
-	    DESedeKeySpec keySpec = new DESedeKeySpec(key);
-	    return SecretKeyFactory.getInstance("DESede").generateSecret(keySpec);
-	} catch (GeneralSecurityException ex) {
-	    throw new InternalError("Can't generate DES key: "+ex);
+    static {
+	for (int i = 0; i < 256; i++) {
+	    int p = (i ^ (i >> 4));
+	    p ^= p >> 2;
+	    p ^= p >> 1;
+	    odd_parity[i] = (byte) (i ^ (p&1) ^ 1);
 	}
     }
 
-    public byte[] md5(byte[] bytes) {
+    private boolean getKey(String password) {
+	byte[] pwbytes;
+	try {
+	    pwbytes = password.getBytes(charSetName);
+	} catch (UnsupportedEncodingException ex) {
+	    throw new InternalError("Encoding not supported");
+	}
+	if (db.getVersion() <= 4)
+	    return getKey4(pwbytes);
+	else
+	    return getKey5(pwbytes);
+    }
+
+    private boolean getKey4(byte[] password) {
+	byte[] pw = new byte[64];
+	System.arraycopy(salt, 0, pw, 0, 4);
+	System.arraycopy(password, 0, pw, 4, Math.min(60,password.length));
+	if (!Arrays.equals(md5(pw), mdigest))
+	    return false;
+
+	byte[] digest = md5(password);
+	byte[] rawkey = new byte[24];
+	for (int i = 0; i < 16; i++) {
+	    rawkey[i] = odd_parity[digest[i] & 0xff];
+	}
+	for (int i = 0; i < 8; i++) {
+	    rawkey[i+16] = rawkey[i];
+	}
+
+
+	try {
+	    cipher = Cipher.getInstance("DESede/ECB/NoPadding");
+	    key = SecretKeyFactory.getInstance("DESede")
+		.generateSecret(new DESedeKeySpec(rawkey));
+	} catch (GeneralSecurityException ex) {
+	    throw new InternalError("Can't get DESede encryption: "
+				    +ex.toString());
+	}
+	return true;
+    }
+    
+    /**
+     * This method is an implementation of PKCS#5 PBKDF2.
+     *
+     * @param pass Database password
+     * @param salt Salt
+     * @param iter Iterations
+     * @param keylen Keylength of choosen cipher type
+     *
+     * @return Record encryption key
+     */
+    public byte[] pbkdf2(byte[] pass, byte[] salt, int iter, int keylen) 
+	throws GeneralSecurityException
+    {
+	// PKCS#5 PBKDF2
+	// Key Derivation function
+	int SHA_DIGEST_LENGTH = 20;
+	int blocklen;
+	int i = 1;
+	byte itmp[] = new byte[4];
+	int pos = 0;
+	byte digtmp[] = new byte[SHA_DIGEST_LENGTH];
+	byte p[] = new byte[keylen];
+	int j, k;
+	
+	Mac mac = Mac.getInstance("HmacSHA1");
+        SecretKeySpec key = new SecretKeySpec(pass, "HmacSHA1");
+        mac.init(key);
+	
+	while(keylen > 0) {
+	    
+	    if(keylen > SHA_DIGEST_LENGTH)
+		blocklen = SHA_DIGEST_LENGTH;
+	    else
+		blocklen = keylen;
+	    
+	    itmp[0] = (byte)(0xff & (i >> 24));
+	    itmp[1] = (byte)(0xff & (i >> 16));
+	    itmp[2] = (byte)(0xff & (i >> 8));
+	    itmp[3] = (byte)(0xff & i);
+
+	    mac.reset();
+	    mac.update(salt);
+	    digtmp = mac.doFinal(itmp);
+	    System.arraycopy(digtmp, 0, p, pos, blocklen);
+	    
+	    for(j = 1; j < iter; j++) {
+		mac.reset();
+		digtmp = mac.doFinal(digtmp);
+
+		for(k = 0; k < blocklen; k++)
+		    p[pos + k] ^= digtmp[k];
+	    }
+	    
+	    keylen = keylen - blocklen;
+	    pos = pos + blocklen;
+	    i++;
+	}
+	
+	return p;
+    }
+    
+//     private static String dump(byte[] b) {
+// 	StringBuffer sb = new StringBuffer();
+// 	for (int i = 0; i < b.length; i++) {
+// 	    sb.append(Integer.toString(b[i]&0xff, 16)).append(" ");
+// 	}
+// 	return sb.toString();
+//     }
+
+    public boolean getKey5(byte[] password) {
+	try {
+// 	    System.err.println("password: "+dump(password));
+// 	    System.err.println("salt: "+dump(salt)+ "iter: "+iter+" ciph: "+ciphnr);
+	    byte[] rawkey = pbkdf2(password, salt, iter, keylens[ciphnr]);
+// 	    System.err.println("rawkey: "+dump(rawkey));
+	    if(ciphnr == 1) { // TripleDES
+		// set odd parity
+		for(int i=0; i<24; i++) {
+		    rawkey[i] = odd_parity[rawkey[i] & 0xff];
+		}
+	    }
+	    MessageDigest md = MessageDigest.getInstance("SHA1");
+	    md.update(rawkey);
+	    md.update(salt);
+	    byte[] shadig = md.digest();
+// 	    System.err.println("shadig: "+dump(shadig));
+// 	    System.err.println("mdigest: "+dump(mdigest));
+	    for (int i = 0; i < mdigest.length; i++) {
+		if (shadig[i] != mdigest[i])
+		    return false;
+	    }
+	
+	    if (ciphnr == 0) {
+		cipher = new NullCipher();
+		key = new SecretKeySpec(rawkey, "NULL");
+	    } else {
+		cipher = Cipher.getInstance(keyalgos[ciphnr]);
+		key = new SecretKeySpec(rawkey, keyalgosShort[ciphnr]);
+	    }
+	} catch (GeneralSecurityException ex) {
+	    throw new InternalError("Can't generate key: "+ex);
+	}
+	return true;
+    }
+
+    private byte[] md5(byte[] bytes) {
 	MessageDigest md;
 	try {
 	    md = MessageDigest.getInstance("MD5");
